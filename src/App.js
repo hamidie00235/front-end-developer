1f8bbfeff0ff9530524407937f02583400705822
hamidie00235 <ahmathisseinh@gmail.com>
2023-03-09 15:44:07 +0300

    basket app filter and map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
+import { React } from "react"
+import {useState} from 'react';
+import { Container ,List,Input,SimpleGrid,ThemeIcon} from '@mantine/core';
+import { IconCircleCheck, IconCircleDashed } from '@tabler/icons-react';
 import './App.css';
+import Card from './components/Card';
+const storeItems=[{
+  name:"Utu",
+  price:20,
+},
 
+{
+  name:"Basket Topu",
+  price:23,
+},
+
+{
+  name:"Cikolata",
+  price:26,
+},
+
+
+
+]
 function App() {
+  let [basketItems,setBasketItems] = useState([]);
+  let filteredItems=basketItems.filter((item) =>item.name.ToLowerCase().indexOf(searchValue.toLowerCase())>=0)
+  let[ searchValue,setSearchValue] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <SimpleGrid cols={3} className="store">
+{storeItems.map(({name}) =>  {
+return ( 
+  <Card 
+    key={name}
+   name={name}
+    onAdd={() => setBasketItems([...basketItems,{name}])}/>
+
+  );
+
+})}
+      </SimpleGrid>
+      <Input.Wrapper label="Arama">
+  <Input onChange={ (e)=> setSearchValue(e.target.value)}/>
+</Input.Wrapper>
+      <List
+      spacing="xs"
+      size="sm"
+      center
+      icon={
+        <ThemeIcon color="teal" size={24} radius="xl">
+          <IconCircleCheck size="1rem" />
+        </ThemeIcon>
+      }
+    >
+      {filteredItems.map(({ name },index) => (
+         <List.Item key={index}>{name}</List.Item>
+      ))}
+     
+      
+    </List>
+    </Container>
   );
 }
 
